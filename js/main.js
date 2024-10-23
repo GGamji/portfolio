76daaf12ac52df606addff90a28acf4d273bbe52
GGamji <wndusrj98@naver.com>
2024-10-23 11:01:54 +0900

    sec3 commit

--- js/main.js
+++ js/main.js
@@ -28,15 +28,6 @@ gsap.to(".img_box", {
 
 
 // Skill
-$(window).ready(function(){
-  draw(90, '.skill_html', '#FC490B');
-  draw(75, '.skill_css', '#72A8D7');
-  draw(60, '.skill_script','#FCB040');
-  draw(55, '.skill_jquery','#0865A6');
-  draw(50, '.skill_photo','#001E36');
-  draw(70, '.skill_illust','#320607');
-});
-
 function draw(max, classname, colorname){
   let i=1;
   const func1 = setInterval(function(){
@@ -51,33 +42,66 @@ function draw(max, classname, colorname){
 
 function color1(i, classname,colorname){
   $(classname).css({
-    "background":"conic-gradient("+colorname+" 0% "+i+"%, #FFF7F0 "+i+"% 100%)"
+    "background":"conic-gradient("+colorname+" 3% "+i+"%, #eee "+i+"% 100%)"
   });
 }
 
-gsap.to(".skill_txt_box", {
-  scrollTrigger: {
-    trigger: ".skill_txt_box",
-    start: "top 40%",
-    end: "bottom 90%",
-    scrub: true,
-    markers: true,
-    animation:draw()
-    // pin: true,
+const skills = [
+  {max: 90, classname: '.skill_html', colorname: '#FC490B'},
+  {max: 75, classname: '.skill_css', colorname: '#72A8D7'},
+  {max: 60, classname: '.skill_script', colorname: '#FCB040'},
+  {max: 55, classname: '.skill_jquery', colorname: '#0865A6'},
+  {max: 50, classname: '.skill_photo', colorname: '#001E36'},
+  {max: 70, classname: '.skill_illust', colorname: '#320607'}
+]
+
+ScrollTrigger.create({
+  trigger: '.skills_content',
+  start: "top 70px",
+  end: "50% 30%",
+  // pin: true,
+  scrub: true,
+  onEnter: () => {
+    skills.forEach(skill =>
+      draw(skill.max, skill.classname, skill.colorname));
   },
-});
+  markers: true,
+  // toggleActions: "play none none reset"
+})
 
 
 
-  // project-swiper
-  const projectSwiper = new Swiper('.project-swiper', {
-    loop: true,
-    autoplay: {
-      delay: 4000,
-    },
-    // loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  })
+// project-swiper
+const projectSwiper = new Swiper('.project-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
+
+// contact
+const ani5 = gsap.timeline();
+
+ani5.from('.contact .t1', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t2', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t3', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t4', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t5', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t6', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t7', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+  .from('.contact .t8', {y: 50, autoAlpha: 0, duration: 2}, '+=1')
+
+ScrollTrigger.create({
+  animation: ani5,
+  trigger: '.contact',
+  start: 'top top',
+  end: 'bottom top',
+  pin: true,
+  anticipatePin: 1,
+  scrub: true,
+})
